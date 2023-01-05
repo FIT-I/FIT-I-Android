32d3ad55535435c0bf1a1b9ae00829f96fe8057a
Yujin <jini1514@soongsil.ac.kr>
2023-01-05 12:06:37 +0900

    feat : tag 버튼 클릭 setOnClickListener #1

--- app/src/main/java/com/example/fit_i/HomeFragment.kt
+++ app/src/main/java/com/example/fit_i/HomeFragment.kt
@@ -1,10 +1,11 @@
 package com.example.fit_i
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.fit_i.databinding.FragmentHomeBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,43 +18,64 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    //lateinit var binding: FragmentHomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
-    }
+        //binding = FragmentHomeBinding.inflate(layoutInflater)
+        val binding = FragmentHomeBinding.inflate(inflater, container, false)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+        //태그 클릭 구현
+        binding.btnPt.setOnClickListener(object : View.OnClickListener {
+            var isBtnPtSelected = false
+            override fun onClick(v: View) {
+                binding.btnPt.isSelected = !binding.btnPt.isSelected
+                isBtnPtSelected = !isBtnPtSelected
             }
+        })
+
+        binding.btnDiet.setOnClickListener(object : View.OnClickListener {
+            var isBtnDietSelected = false
+            override fun onClick(v: View) {
+                binding.btnDiet.isSelected = !binding.btnDiet.isSelected
+                isBtnDietSelected = !isBtnDietSelected
+            }
+        })
+
+        binding.btnEating.setOnClickListener(object : View.OnClickListener {
+            var isBtnEatingSelected = false
+            override fun onClick(v: View) {
+                binding.btnEating.isSelected = !binding.btnEating.isSelected
+                isBtnEatingSelected = !isBtnEatingSelected
+            }
+        })
+
+        binding.btnMedical.setOnClickListener(object : View.OnClickListener {
+            var isBtnMedicalSelected = false
+            override fun onClick(v: View) {
+                binding.btnMedical.isSelected = !binding.btnMedical.isSelected
+                isBtnMedicalSelected = !isBtnMedicalSelected
+            }
+        })
+
+        binding.btnFriend.setOnClickListener(object : View.OnClickListener {
+            var isBtnFriendSelected = false
+            override fun onClick(v: View) {
+                binding.btnFriend.isSelected = !binding.btnFriend.isSelected
+                isBtnFriendSelected = !isBtnFriendSelected
+            }
+        })
+
+        // Inflate the layout for this fragment
+        //return inflater.inflate(R.layout.fragment_home, container, false)
+
+        return binding.root
     }
 }
