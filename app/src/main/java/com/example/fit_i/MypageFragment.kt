abbb8dc6cef6629a605f9d0e4e2325419510141c
Yujin <jini1514@soongsil.ac.kr>
2023-01-10 10:14:15 +0900

    fix : 잔 오류 수정 #1

--- app/src/main/java/com/example/fit_i/MypageFragment.kt
+++ app/src/main/java/com/example/fit_i/MypageFragment.kt
@@ -1,27 +1,19 @@
 package com.example.fit_i
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.ImageButton
-import android.widget.TextView
 
 class MypageFragment : Fragment() {
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-        val view = inflater.inflate(R.layout.fragment_mypage, container, false)
-        val goSetting: Button = view.findViewById(R.id.ib_setting)
-
-
-        return view
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_mypage, container, false)
     }
-
 }
