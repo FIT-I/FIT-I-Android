d1d3de0c61b6941bc64b51c15fd690e06b223bdc
Yujin <jini1514@soongsil.ac.kr>
2023-01-13 11:40:16 +0900

    feat : 이용약관 notworking..2 #7

--- app/src/main/java/com/example/fit_i/PermissionActivity.kt
+++ app/src/main/java/com/example/fit_i/PermissionActivity.kt
@@ -3,6 +3,8 @@ package com.example.fit_i
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.CheckBox
 import android.widget.CompoundButton
 import android.widget.TextView
 import com.example.fit_i.databinding.ActivityPermissionBinding
@@ -10,6 +12,10 @@ import com.example.fit_i.databinding.ActivityPermissionBinding
 class PermissionActivity : AppCompatActivity() {
     private lateinit var binding: ActivityPermissionBinding
 
+    private lateinit var allCheckBtn :CheckBox
+    private lateinit var firstCheckBtn :CheckBox
+    private lateinit var secondCheckBtn :CheckBox
+    private lateinit var thirdCheckBtn :CheckBox
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,61 +30,41 @@ class PermissionActivity : AppCompatActivity() {
             finish()
         }
 
-/*
-        val listener = CompoundButton.OnCheckedChangeListener { buttonView, isChecked ->
-            if (isChecked) {
-                when(buttonView.id) {
-                    R.id.cbAll -> { checkedToast(cb1) }
-                    R.id.cb1 -> { checkedToast(cb1) }
-                    R.id.cb2 -> { checkedToast(cb2) }
-                    R.id.cb3 -> { checkedToast(cb3) }
-                }
-            }
-            else {
-                .
-                .
-                .
-            }
-        }
+        allCheckBtn = binding.cbAll
+        firstCheckBtn = binding.cb1
+        secondCheckBtn = binding.cb2
+        thirdCheckBtn = binding.cb3
 
-        binding.cbAll.setOnCheckedChangeListener(listener)
-        binding.cb1.setOnCheckedChangeListener(listener)
-        binding.cb2.setOnCheckedChangeListener(listener)
-        binding.cb3.setOnCheckedChangeListener(listener)
-*/
-        //binding.cbAll.setOnClickListener{ onCheckChanged(binding.cbAll)}
-
-        val allCheckBtn = binding.cbAll
-        val firstCheckBtn = binding.cb1
-        val secondCheckBtn = binding.cb2
-        val thirdCheckBtn = binding.cb3
-
-        allCheckBtn.setOnClickListener { onCheckChanged(allCheckBtn) }
-        firstCheckBtn.setOnClickListener { onCheckChanged(firstCheckBtn) }
-        secondCheckBtn.setOnClickListener { onCheckChanged(secondCheckBtn) }
-        thirdCheckBtn.setOnClickListener { onCheckChanged(thirdCheckBtn) }
+        allCheckBtn.setOnClickListener {
+            onCheckChanged(allCheckBtn)
+        Log.d("cb","all")}
+        firstCheckBtn.setOnClickListener { onCheckChanged(firstCheckBtn)
+            Log.d("cb","1") }
+        secondCheckBtn.setOnClickListener { onCheckChanged(secondCheckBtn)
+            Log.d("cb","2")}
+        thirdCheckBtn.setOnClickListener { onCheckChanged(thirdCheckBtn)
+            Log.d("cb","3")}
 
     }
 
     private fun onCheckChanged(compoundButton: CompoundButton) {
         when(compoundButton.id) {
             R.id.cbAll -> {
-                if (binding.cbAll.isChecked) {
-                    binding.cb1.isChecked = true
-                    binding.cb2.isChecked = true
-                    binding.cb3.isChecked = true
+                if (allCheckBtn.isChecked) {
+                    firstCheckBtn.isChecked = true
+                    secondCheckBtn.isChecked = true
+                    thirdCheckBtn.isChecked = true
                 }else {
-                    binding.cbAll.isChecked = false
-                    binding.cb1.isChecked = false
-                    binding.cb2.isChecked = false
-                    binding.cb3.isChecked = false
+                    firstCheckBtn.isChecked = false
+                    secondCheckBtn.isChecked = false
+                    thirdCheckBtn.isChecked = false
                 }
             }
             else -> {
-                binding.cbAll.isChecked = (
-                        binding.cb1.isChecked
-                                && binding.cb2.isChecked
-                                && binding.cb3.isChecked)
+                allCheckBtn.isChecked = (
+                        firstCheckBtn.isChecked
+                                && secondCheckBtn.isChecked
+                                && thirdCheckBtn.isChecked)
             }
         }
     }
