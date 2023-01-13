9b334736471dc48937492c7866d789a9d5b7347e
Yujin <jini1514@soongsil.ac.kr>
2023-01-13 18:12:03 +0900

    feat&add : 회원가입, 비밀번호 찾기 UI수정 #7

---
+++ app/src/main/java/com/example/fit_i/FindPw2Activity.kt
@@ -0,0 +1,11 @@
+package com.example.fit_i
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FindPw2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_find_pw2)
+    }
+}
