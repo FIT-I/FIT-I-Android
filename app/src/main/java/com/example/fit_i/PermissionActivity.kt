23ecca12221b11aa3c332915485d68aa88c42a1e
Yujin <jini1514@soongsil.ac.kr>
2023-01-15 00:18:52 +0900

    feat : 이용약관 동의 체크박스 알고리즘 & intent 연결 #18

--- app/src/main/java/com/example/fit_i/PermissionActivity.kt
+++ app/src/main/java/com/example/fit_i/PermissionActivity.kt
@@ -4,47 +4,43 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.CheckBox
 import android.widget.CompoundButton
 import android.widget.TextView
 import com.example.fit_i.databinding.ActivityPermissionBinding
 
 class PermissionActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityPermissionBinding
+//    private lateinit var binding: ActivityPermissionBinding
 
-    private lateinit var allCheckBtn :CheckBox
-    private lateinit var firstCheckBtn :CheckBox
-    private lateinit var secondCheckBtn :CheckBox
-    private lateinit var thirdCheckBtn :CheckBox
+    private lateinit var allCheckBtn : CheckBox
+    private lateinit var firstCheckBtn : CheckBox
+    private lateinit var secondCheckBtn : CheckBox
+    private lateinit var thirdCheckBtn : CheckBox
+    private lateinit var btnGoSignIn : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_permission)
-        binding = ActivityPermissionBinding.inflate(layoutInflater)
 
-        //회원가입하기
-        val signIn = findViewById<TextView>(R.id.btn_go_signIn)
-        signIn.setOnClickListener {
-            val intent = Intent(this, SigninActivity::class.java)
+
+        allCheckBtn = findViewById(R.id.cbAll)
+        firstCheckBtn=findViewById(R.id.cb1)
+        secondCheckBtn= findViewById(R.id.cb2)
+        thirdCheckBtn = findViewById(R.id.cb3)
+        btnGoSignIn= findViewById(R.id.btn_go_signIn)
+        btnGoSignIn.isEnabled = false
+
+        allCheckBtn.setOnClickListener { onCheckChanged(allCheckBtn) }
+        firstCheckBtn.setOnClickListener { onCheckChanged(firstCheckBtn) }
+        secondCheckBtn.setOnClickListener { onCheckChanged(secondCheckBtn) }
+        thirdCheckBtn.setOnClickListener { onCheckChanged(thirdCheckBtn) }
+
+        btnGoSignIn.setOnClickListener(){
+            val intent = Intent(this, SignupActivity::class.java)
             startActivity(intent)  // 화면 전환을 시켜줌
             finish()
         }
-
-        allCheckBtn = binding.cbAll
-        firstCheckBtn = binding.cb1
-        secondCheckBtn = binding.cb2
-        thirdCheckBtn = binding.cb3
-
-        allCheckBtn.setOnClickListener {
-            onCheckChanged(allCheckBtn)
-        Log.d("cb","all")}
-        firstCheckBtn.setOnClickListener { onCheckChanged(firstCheckBtn)
-            Log.d("cb","1") }
-        secondCheckBtn.setOnClickListener { onCheckChanged(secondCheckBtn)
-            Log.d("cb","2")}
-        thirdCheckBtn.setOnClickListener { onCheckChanged(thirdCheckBtn)
-            Log.d("cb","3")}
-
     }
 
     private fun onCheckChanged(compoundButton: CompoundButton) {
@@ -54,6 +50,9 @@ class PermissionActivity : AppCompatActivity() {
                     firstCheckBtn.isChecked = true
                     secondCheckBtn.isChecked = true
                     thirdCheckBtn.isChecked = true
+
+                    btnGoSignIn.isEnabled = true
+
                 }else {
                     firstCheckBtn.isChecked = false
                     secondCheckBtn.isChecked = false
@@ -64,8 +63,13 @@ class PermissionActivity : AppCompatActivity() {
                 allCheckBtn.isChecked = (
                         firstCheckBtn.isChecked
                                 && secondCheckBtn.isChecked
-                                && thirdCheckBtn.isChecked)
-            }
+                                && thirdCheckBtn.isChecked )}
         }
+
+        btnGoSignIn.isEnabled = allCheckBtn.isChecked
     }
+
+//    private fun isTrue(): Boolean {
+//        return name.isNotEmpty()&&email.isNotEmpty()&&pw.isNotEmpty()&&pw2.isNotEmpty()
+//    }
 }
