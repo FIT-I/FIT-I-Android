186702cf88fab3757b464c6bf0124370cfd5a5c1
gurino <2021111786@dgu.ac.kr>
2023-01-16 05:03:38 +0900

    feat : myppage2 profile ui 완성 #19

--- app/src/main/java/com/example/fit_i/MainActivity.kt
+++ app/src/main/java/com/example/fit_i/MainActivity.kt
@@ -39,7 +39,17 @@ class MainActivity : AppCompatActivity() {
                     val mypageFragment = MypageFragment()
                     supportFragmentManager.beginTransaction()
                         .replace(R.id.fl_container, mypageFragment).commit()
+                    val mypageSettingFragment = MypageSettingFragment()
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fl_container, mypageSettingFragment).commit()
+                    val mypageSiginoutFragment = MypageSiginoutFragment()
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fl_container, mypageSiginoutFragment).commit()
+                    val mypageLikelistFragment = MypageLikelistFragment()
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fl_container, mypageLikelistFragment).commit()
                 }
+
             }
             true
         }
--- app/src/main/java/com/example/fit_i/MypageFragment.kt
+++ app/src/main/java/com/example/fit_i/MypageFragment.kt
@@ -1,6 +1,7 @@
 package com.example.fit_i
 
 import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Im
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +9,7 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
 
@@ -24,7 +26,12 @@ class MypageFragment : Fragment() {
         val vv = inflater.inflate(R.layout.fragment_mypage, container, false)
         val ibsetting = vv.findViewById<View>(R.id.ib_setting) as ImageButton
         val ivnextlike = vv.findViewById<View>(R.id.iv_next_like) as ImageView
+        val ivnextnotice = vv.findViewById<View>(R.id.iv_next_notice) as ImageView
+        val ivnextpermisson = vv.findViewById<View>(R.id.iv_next_permisson) as ImageView
+        val tvgotoprofile = vv.findViewById<View>(R.id.tv_go_modifyProfile) as TextView
 
+
+        //설정
         ibsetting.setOnClickListener {
             val mypageSettingFragment = MypageSettingFragment()
             val transaction: FragmentTransaction = requireFragmentManager().beginTransaction()
@@ -33,6 +40,7 @@ class MypageFragment : Fragment() {
             transaction.commit()
         }
 
+        //찜 목록
         ivnextlike.setOnClickListener {
             val mypageLikelistFragment = MypageLikelistFragment()
             val transaction : FragmentTransaction = requireFragmentManager().beginTransaction()
@@ -40,6 +48,31 @@ class MypageFragment : Fragment() {
             transaction.replace(R.id.fl_container, mypageLikelistFragment)
             transaction.commit()
         }
+        // 공지사항
+        ivnextnotice.setOnClickListener {
+            val mypageLikelistFragment = MypageLikelistFragment()
+            val transaction : FragmentTransaction = requireFragmentManager().beginTransaction()
+
+            transaction.replace(R.id.fl_container, mypageLikelistFragment)
+            transaction.commit()
+        }
+        //프로필 수정
+        tvgotoprofile.setOnClickListener {
+            val mypageLikelistFragment = MypageLikelistFragment()
+            val transaction : FragmentTransaction = requireFragmentManager().beginTransaction()
+
+            transaction.replace(R.id.fl_container, mypageLikelistFragment)
+            transaction.commit()
+        }
+        //이용약관
+        ivnextpermisson.setOnClickListener {
+            val mypageLikelistFragment = MypageLikelistFragment()
+            val transaction : FragmentTransaction = requireFragmentManager().beginTransaction()
+
+            transaction.replace(R.id.fl_container, mypageLikelistFragment)
+            transaction.commit()
+        }
+
 
 
         return vv
