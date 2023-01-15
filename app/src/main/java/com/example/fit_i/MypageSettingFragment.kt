92ca2afae72d122b57c90c4be83847f9399d57e8
gurino <2021111786@dgu.ac.kr>
2023-01-16 02:14:50 +0900

    add : dialog 팝업창 띄우기 #19

--- app/src/main/java/com/example/fit_i/MypageSettingFragment.kt
+++ app/src/main/java/com/example/fit_i/MypageSettingFragment.kt
@@ -1,18 +1,19 @@
 package com.example.fit_i
 
+import android.app.Activity
 import android.app.Dialog
 import android.os.Bundle
+import android.os.ProxyFileDescriptorCallback
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
 
-class MypageSettingFragment : Fragment() {
-
-
-
+class MypageSettingFragment :Fragment() {
+    private lateinit var binding: MypageSettingFragment
 
 
     override fun onCreateView(
@@ -20,17 +21,22 @@ class MypageSettingFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        val ee = inflater.inflate(R.layout.fragment_mypage_setting,container,false)
+        val ee = inflater.inflate(R.layout.fragment_mypage_setting, container, false)
         val btnlogout = ee.findViewById<View>(R.id.btn_logout) as Button
 
         btnlogout.setOnClickListener {
-            val dialogFragment = DialogFragment()
-            val transaction:FragmentTransaction = requireFragmentManager().beginTransaction()
-
-            transaction.replace(R.id.fl_container,dialogFragment)
-            transaction.commit()
+            showProfileDialog()
         }
         return ee
+
+
+}
+
+    private fun showProfileDialog() {
+        ProfileDialog(requireContext()){
+
+        }.show()
     }
 
 }
+
