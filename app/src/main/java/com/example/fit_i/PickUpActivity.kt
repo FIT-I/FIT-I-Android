9f9b1071289ad1cb539c283c334afadbad336159
yoon-gang00 <sgi07151@naver.com>
2023-01-16 20:57:05 +0900

    Feat: 버튼누를때 색바뀌는것 #3

--- app/src/main/java/com/example/fit_i/PickUpActivity.kt
+++ app/src/main/java/com/example/fit_i/PickUpActivity.kt
@@ -2,15 +2,31 @@ package com.example.fit_i
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.CompoundButton
 import android.widget.ImageButton
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 
 class PickUpActivity :AppCompatActivity(){
+    private lateinit var pickup1 : CheckBox
+    private lateinit var pickup2 : CheckBox
+    private lateinit var buttonNext : Button
+
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pickup)
+
+        pickup1 = findViewById(R.id.pickup1_btn)
+        pickup2 = findViewById(R.id.pickup2_btn)
+        buttonNext = findViewById(R.id.button_next)
+        buttonNext.isEnabled = false
+
+        pickup1.setOnClickListener{onCheckChanged(pickup1)}
+        pickup2.setOnClickListener{onCheckChanged(pickup2)}
+
         //뒤로가기
         val goBack = findViewById<ImageButton>(R.id.back_arrow)
         goBack.setOnClickListener{
@@ -28,5 +44,23 @@ class PickUpActivity :AppCompatActivity(){
 
 
     }
+    private fun onCheckChanged(compoundButton: CompoundButton){
+        when(compoundButton.id){
+            R.id.pickup1_btn-> {
+                if (pickup1.isChecked) {
+                    pickup2.isChecked = false
+                    buttonNext.isEnabled = true
+                }
+            }
+            R.id.pickup2_btn->{
+                if(pickup2.isChecked){
+                    pickup1.isChecked = false
+                    buttonNext.isEnabled = true
+                }
+            }
 
+        }
+        buttonNext.isEnabled = pickup1.isChecked
+        buttonNext.isEnabled = pickup2.isChecked
+    }
 }
--- app/src/main/java/com/example/fit_i/ServiceActivity.kt
+++ app/src/main/java/com/example/fit_i/ServiceActivity.kt
@@ -3,13 +3,26 @@ package com.example.fit_i
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.CompoundButton
 import android.widget.ImageButton
 import android.widget.TextView
 
 class ServiceActivity :AppCompatActivity(){
+    private lateinit var checkBox: CheckBox
+    private lateinit var buttonNext : Button
     override fun onCreate(savedInstanceState:Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_service)
+
+        checkBox = findViewById(R.id.checkBox)
+        buttonNext=findViewById(R.id.button_next)
+        buttonNext.isEnabled = false
+
+        checkBox.setOnClickListener{ onCheckChanged(checkBox)}
+
+
         //뒤로가기
         val goBack = findViewById<ImageButton>(R.id.back_arrow)
         goBack.setOnClickListener{
@@ -17,12 +30,26 @@ class ServiceActivity :AppCompatActivity(){
             startActivity(intent)
             finish()
         }
+
         //서비스선택 다음단계로 이동
-        val nextStep = findViewById<TextView>(R.id.button_next)
-        nextStep.setOnClickListener{
+
+
+        buttonNext.setOnClickListener{
             val intent = Intent(this,DateChooseActivity::class.java)
             startActivity(intent)
             finish()
         }
+    }
+    private fun onCheckChanged(compoundButton: CompoundButton){
+        when(compoundButton.id){
+            R.id.checkBox->{
+            if(checkBox.isChecked){
+                buttonNext.isEnabled = true
+            }
+            }
+        }
+
+        buttonNext.isEnabled = checkBox.isChecked
+
     }
 }
