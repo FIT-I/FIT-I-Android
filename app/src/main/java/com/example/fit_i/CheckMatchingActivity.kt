e93067ef25bf3316a738b9bfab6afb086b2efce3
yoon-gang00 <sgi07151@naver.com>
2023-01-17 20:17:04 +0900

    Feat: 매칭요청 팝업창 기능 추가 #3

--- app/src/main/java/com/example/fit_i/CheckMatchingActivity.kt
+++ app/src/main/java/com/example/fit_i/CheckMatchingActivity.kt
@@ -3,6 +3,10 @@ package com.example.fit_i
 import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
+import android.text.Layout
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageButton
 import androidx.appcompat.app.AlertDialog
@@ -16,31 +20,31 @@ class CheckMatchingActivity :AppCompatActivity(){
 
         binding = ActivityCheckMatching1Binding.inflate(layoutInflater)
         setContentView(R.layout.activity_check_matching1)
+
+        val btnNext: Button = findViewById(R.id.btn_next)
+        btnNext.setOnClickListener{
+            showDialog()
+        }
     //뒤로가기
         val goBack = findViewById<ImageButton>(R.id.back_arrow)
         goBack.setOnClickListener{
             val intent = Intent(this,PickUpActivity::class.java)
             startActivity(intent)
             finish()
-        }
 
-        binding.btnNext.setOnClickListener{
-            val builder = AlertDialog.Builder(this)
-            builder
-                .setTitle("매칭요청")
-                .setMessage("매칭을 요청하시겠습니까?")
-//                .setPositiveButton("요청",
-//                    DialogInterface.OnClickListener { dialog:DialogInterface, id ->
-                        // 요청 버튼 선택 시 수행
-//                    })
-//                .setNegativeButton("취소",
-//                    DialogInterface.OnClickListener { dialog, id ->
-                        // 취소 버튼 선택 시 수행
-//                    })
-// Create the AlertDialog object and return it
-            builder.create()
-            builder.show()
         }
 
     }
+    private fun showDialog(){
+        val builder : AlertDialog.Builder = AlertDialog.Builder(this)
+
+
+
+        val inflater : LayoutInflater = layoutInflater
+        builder.setView(inflater.inflate(R.layout.dialog_match_request, null))
+
+        val alertDialog: AlertDialog = builder.create()
+        alertDialog.show()
+    }
+
 }
