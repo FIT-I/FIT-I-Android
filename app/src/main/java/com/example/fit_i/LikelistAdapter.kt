9030e6bc9191cf96e28ac1c860c069846b65da4f
gurino <2021111786@dgu.ac.kr>
2023-01-23 14:43:23 +0900

    feat : 찜목록 이동 #19

--- app/src/main/java/com/example/fit_i/LikelistAdapter.kt
+++ app/src/main/java/com/example/fit_i/LikelistAdapter.kt
@@ -8,66 +8,59 @@ import android.widget.AdapterView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.fit_i.databinding.ItemLikelistBinding
 
-class LikelistAdapter(private val dataList: ArrayList<LikelistData>):
-RecyclerView.Adapter<LikelistAdapter.ViewHolder>(){
-    inner class ViewHolder(private val binding: ItemLikelistBinding):
-            RecyclerView.ViewHolder(binding.root){
+class LikelistAdapter(private val dataList: ArrayList<LikelistData>) :
+    RecyclerView.Adapter<LikelistAdapter.ViewHolder>() {
+    //1 리스트 내 항목 클릭 시 onClick 호출
+    override fun onBindViewHolder(holder: ViewHolder, position: Int, payloads: MutableList<Any>) {
+        super.onBindViewHolder(holder, position, payloads)
+        holder.itemView.setOnClickListener{
+            itemClickListener.onClick(it,position)
+        }
+    }
+     // 2 리스너 인터페이스
+     interface OnItemClickListener {
+     fun onClick(v: View, position: Int)
+     }
 
-
-
-                fun onBind(position: Int){
-                    binding.matchIm.text = dataList[position].im
-                    binding.matchStar.text = dataList[position].star
-                    binding.matchUni.text = dataList[position].uni
-
-                    itemView.setOnClickListener{
-                        Log.d("Click","success")
-
-                    }
-//                    val pos = adapterPosition
-//                    if(pos!= RecyclerView.NO_POSITION)
-//                    {
-//                        itemView.setOnClickListener {
-//                            listener?.onItemClick(itemView,item,pos)
-//                        }
-//                    }
-
-                }
-
-            }
-    interface OnItemClicklistener : AdapterView.OnItemClickListener{
-        fun onItemClick(v:View, data: LikelistData, position: Int)
+     // 3 외부에서 클릭 시 이벤트 설정
+    fun setItemClickListener(onItemClickListener: OnItemClickListener) {
+    this.itemClickListener = onItemClickListener
     }
 
-//    interface OnItemClickListener{
-//        fun onItemClick(v:View, data: ProfileData, pos : Int)
-//    }
-//    private var listener : OnItemClickListener? = null
-//    fun setOnItemClickListener(listener : OnItemClickListener) {
-//        this.listener = listener
-//    }
+    // 4 setItemClickListener로 설정한 함수 설정
+
+    private lateinit var itemClickListener: OnItemClickListener
+
+//작동방식은 itemClickListener(어댑터) <-> OnItemClickListener <-> setItemClickListener(액티비티)
+
+inner class ViewHolder(private val binding: ItemLikelistBinding) :
+    RecyclerView.ViewHolder(binding.root) {
 
 
-//넣을 액티비티나 프래그먼트에 추가하기
-//    profileAdapter.setOnItemClickListener(object : ProfileAdapter.OnItemClickListener{
-//        override fun onItemClick(v: View, data: ProfileData, pos : Int) {
-//            Intent(this@MainActivity, ProfileDetailActivity::class.java).apply {
-//                putExtra("data", data)
-//                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-//            }.run { startActivity(this) }
-//        }
-//
-//    })
+    fun onBind(position: Int) {
+        binding.matchIm.text = dataList[position].im
+        binding.matchStar.text = dataList[position].star
+        binding.matchUni.text = dataList[position].uni
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val viewBinding = ItemLikelistBinding.inflate(LayoutInflater.from(parent.context),parent,false)
-        return ViewHolder(viewBinding)
+        itemView.setOnClickListener {
+            Log.d("Click", "success")
+
+        }
+
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.onBind(position)
-    }
+}
 
-    override fun getItemCount(): Int= dataList.size
+override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+    val viewBinding =
+        ItemLikelistBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+    return ViewHolder(viewBinding)
+}
+
+override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    holder.onBind(position)
+}
+
+override fun getItemCount(): Int = dataList.size
 
 }
