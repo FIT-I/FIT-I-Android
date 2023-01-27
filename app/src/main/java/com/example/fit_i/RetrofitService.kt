9d13cc46d6bcbe32ed9636a84cbcdf633064abdc
yoon-gang00 <sgi07151@naver.com>
2023-01-27 15:14:11 +0900

    Refactoring : 머지전 확인 #3

--- app/src/main/java/com/example/fit_i/RetrofitService.kt
+++ app/src/main/java/com/example/fit_i/RetrofitService.kt
@@ -1,5 +1,6 @@
 package com.example.fit_i
 
+import com.google.firebase.firestore.auth.User
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.GET
@@ -21,4 +22,8 @@ interface RetrofitService {
 
     @GET("posts/{page}")
     fun getUserPage(@Path("page") page: String): Call<User>
+
+    //찜목록조회
+//    @GET("api/customer/wish")
+//    fun getCustomerWish(@Path())
 }
