0335d504077ccfbb93faed45c432274aed34e655
Yujin <jini1514@soongsil.ac.kr>
2023-01-30 12:11:00 +0900

    feat : 비밀번호 찾기 #32

--- app/src/main/java/com/example/fit_i/ui/login/LoginFindPwActivity.kt
+++ app/src/main/java/com/example/fit_i/ui/login/LoginFindPwActivity.kt
@@ -1,17 +1,26 @@
-package com.example.fit_i
+package com.example.fit_i.ui.login
 
 import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.fit_i.R
+import com.example.fit_i.RetrofitImpl
+import com.example.fit_i.data.model.request.ChangePWRequest
+import com.example.fit_i.data.model.response.BaseResponse
+import com.example.fit_i.data.service.AccountsService
 import com.example.fit_i.databinding.ActivityFindPwBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-class FindPwActivity : AppCompatActivity() {
+class LoginFindPwActivity : AppCompatActivity() {
     private lateinit var binding: ActivityFindPwBinding
 
     var emailF: String=""
@@ -40,7 +49,6 @@ class FindPwActivity : AppCompatActivity() {
                 //입력값 담기
                 emailF = etEmailF.text.toString()
 
-
                 //stroke 색상변경
                 if(emailF.isNotEmpty())
                     etEmailF.setBackgroundResource(R.drawable.edittext_border)
@@ -52,7 +60,27 @@ class FindPwActivity : AppCompatActivity() {
 
                 btnFindPw.setOnClickListener(){
                     if(btnFindPw.isEnabled)
-                        etTempPwF.visibility= View.VISIBLE}
+                        etTempPwF.visibility= View.VISIBLE
+
+                    val service = RetrofitImpl.getApiClient().create(AccountsService::class.java)
+                    service.sendEmail(emailF).enqueue(object : Callback<BaseResponse> {
+                        override fun onResponse(call: Call<BaseResponse>, response: Response<BaseResponse>) {
+                            if(response.isSuccessful){
+                                // 정상적으로 통신이 성공된 경우
+                                Log.d("post", "onResponse 성공: " + response.body().toString());
+                                Toast.makeText(this@LoginFindPwActivity, "이메일이 전송되었습니다.", Toast.LENGTH_SHORT).show()
+                            }else{
+                                // 통신이 실패한 경우(응답코드 3xx, 4xx 등)
+                                Log.d("post", "onResponse 실패")
+                            }
+                        }
+
+                        override fun onFailure(call: Call<BaseResponse>, t: Throwable) {
+                            // 통신 실패 (인터넷 끊킴, 예외 발생 등 시스템적인 이유)
+                            Log.d("post", "onFailure 에러: " + t.message.toString());
+                        }
+                    })
+                }
             }
             override fun afterTextChanged(p0: Editable?) {}
         })
@@ -67,13 +95,11 @@ class FindPwActivity : AppCompatActivity() {
                 if (btnFindPw.text=="임시 비밀번호 발급")
                         btnFindPw.text="로그인"
 
-
                 //stroke 색상변경
                 if(tempPW.isNotEmpty())
                     etTempPwF.setBackgroundResource(R.drawable.edittext_border)
                 else
                     etTempPwF.setBackgroundResource(R.drawable.edittext_border_not)
-
 
                 btnFindPw.setOnClickListener {
                     letgo()
@@ -88,5 +114,25 @@ class FindPwActivity : AppCompatActivity() {
         startActivity(intent)  // 화면 전환을 시켜줌
         finish()
         Toast.makeText(this, emailF + "findPW", Toast.LENGTH_SHORT).show()
+
+
+        val service = RetrofitImpl.getApiClient().create(AccountsService::class.java)
+        service.findPW(emailF).enqueue(object : Callback<BaseResponse> {
+            override fun onResponse(call: Call<BaseResponse>, response: Response<BaseResponse>) {
+                if(response.isSuccessful){
+                    // 정상적으로 통신이 성공된 경우
+                    Log.d("post", "onResponse 성공: " + response.body().toString());
+                    Toast.makeText(this@LoginFindPwActivity, "비밀번호 찾기 성공!", Toast.LENGTH_SHORT).show()
+                }else{
+                    // 통신이 실패한 경우(응답코드 3xx, 4xx 등)
+                    Log.d("post", "onResponse 실패")
+                }
+            }
+
+            override fun onFailure(call: Call<BaseResponse>, t: Throwable) {
+                // 통신 실패 (인터넷 끊킴, 예외 발생 등 시스템적인 이유)
+                Log.d("post", "onFailure 에러: " + t.message.toString());
+            }
+        })
     }
 }
