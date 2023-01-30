ae4b10bcae30569e47a183ed80b9a28c12a2887c
Yujin <jini1514@soongsil.ac.kr>
2023-01-30 14:49:28 +0900

    feat : 비밀번호 찾기 api 연결 끝 #32

--- app/src/main/java/com/example/fit_i/ui/login/LoginFindPwActivity.kt
+++ app/src/main/java/com/example/fit_i/ui/login/LoginFindPwActivity.kt
@@ -1,6 +1,7 @@
 package com.example.fit_i.ui.login
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
@@ -8,11 +9,11 @@ import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.fit_i.R
 import com.example.fit_i.RetrofitImpl
-import com.example.fit_i.data.model.request.ChangePWRequest
 import com.example.fit_i.data.model.response.BaseResponse
 import com.example.fit_i.data.service.AccountsService
 import com.example.fit_i.databinding.ActivityFindPwBinding
@@ -24,7 +25,9 @@ class LoginFindPwActivity : AppCompatActivity() {
     private lateinit var binding: ActivityFindPwBinding
 
     var emailF: String=""
-    var tempPW:String=""
+    var codeF:String=""
+    var returnCode : String =""
+    lateinit var codeWrongMessage : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,13 +36,15 @@ class LoginFindPwActivity : AppCompatActivity() {
         binding = ActivityFindPwBinding.inflate(layoutInflater)
 
         val etEmailF : EditText= findViewById(R.id.et_emailF)
-        val etTempPwF :EditText = findViewById(R.id.et_tempPwF)
+        val etCodeF :EditText = findViewById(R.id.et_tempPwF)
         val btnFindPw : Button = findViewById(R.id.btn_find_pw)
+        codeWrongMessage = findViewById(R.id.tv_wrong_code)
 
         btnFindPw.isEnabled = false
         btnFindPw.text="인증코드 받기"
 
-        etTempPwF.visibility= View.INVISIBLE
+        etCodeF.visibility= View.INVISIBLE
+        codeWrongMessage.visibility = View.INVISIBLE
 
         etEmailF.addTextChangedListener(object: TextWatcher {
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
@@ -60,15 +65,16 @@ class LoginFindPwActivity : AppCompatActivity() {
 
                 btnFindPw.setOnClickListener(){
                     if(btnFindPw.isEnabled)
-                        etTempPwF.visibility= View.VISIBLE
+                        etCodeF.visibility= View.VISIBLE
 
-                    val service = RetrofitImpl.getApiClient().create(AccountsService::class.java)
-                    service.sendEmail(emailF).enqueue(object : Callback<BaseResponse> {
+                    val accountService = RetrofitImpl.getApiClient().create(AccountsService::class.java)
+                    accountService.sendEmail(emailF).enqueue(object : Callback<BaseResponse> {
                         override fun onResponse(call: Call<BaseResponse>, response: Response<BaseResponse>) {
                             if(response.isSuccessful){
                                 // 정상적으로 통신이 성공된 경우
                                 Log.d("post", "onResponse 성공: " + response.body().toString());
                                 Toast.makeText(this@LoginFindPwActivity, "이메일이 전송되었습니다.", Toast.LENGTH_SHORT).show()
+                                returnCode= response.body()?.result.toString()
                             }else{
                                 // 통신이 실패한 경우(응답코드 3xx, 4xx 등)
                                 Log.d("post", "onResponse 실패")
@@ -85,21 +91,21 @@ class LoginFindPwActivity : AppCompatActivity() {
             override fun afterTextChanged(p0: Editable?) {}
         })
 
-        etTempPwF.addTextChangedListener(object: TextWatcher {
+        etCodeF.addTextChangedListener(object: TextWatcher {
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
 
             //값 변경 시 실행되는 함수
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
                 //입력값 담기
-                tempPW = etTempPwF.text.toString()
+                codeF = etCodeF.text.toString()
                 if (btnFindPw.text=="인증코드 받기")
                         btnFindPw.text="완료"
 
                 //stroke 색상변경
-                if(tempPW.isNotEmpty())
-                    etTempPwF.setBackgroundResource(R.drawable.edittext_border)
+                if(codeF.isNotEmpty())
+                    etCodeF.setBackgroundResource(R.drawable.edittext_border)
                 else
-                    etTempPwF.setBackgroundResource(R.drawable.edittext_border_not)
+                    etCodeF.setBackgroundResource(R.drawable.edittext_border_not)
 
                 btnFindPw.setOnClickListener {
                     letgo()
@@ -111,20 +117,26 @@ class LoginFindPwActivity : AppCompatActivity() {
 
     private fun letgo() {
         val intent = Intent(this, LoginSplashActivity::class.java)
-        startActivity(intent)  // 화면 전환을 시켜줌
-        finish()
-        Toast.makeText(this, emailF + "findPW", Toast.LENGTH_SHORT).show()
-
 
         val service = RetrofitImpl.getApiClient().create(AccountsService::class.java)
         service.findPW(emailF).enqueue(object : Callback<BaseResponse> {
             override fun onResponse(call: Call<BaseResponse>, response: Response<BaseResponse>) {
                 if(response.isSuccessful){
                     // 정상적으로 통신이 성공된 경우
-                    Log.d("post", "onResponse 성공: " + response.body().toString());
-                    Toast.makeText(this@LoginFindPwActivity, response.body()?.result.toString()+" 비밀번호 찾기 성공!", Toast.LENGTH_SHORT).show()
+                    if(codeF== returnCode){
+                        codeWrongMessage.visibility = View.INVISIBLE
 
-                    //(인증코드가 잘못되었습니다. 다시 확인해 주세요)
+                        Toast.makeText(this@LoginFindPwActivity,
+                            "비밀번호 찾기 성공!", Toast.LENGTH_SHORT).show()
+                        Log.d("post", "onResponse 성공: " + response.body().toString());
+                        startActivity(intent)  // 화면 전환을 시켜줌
+                        finish()
+                    }
+                    else{//(인증코드가 잘못되었습니다. 다시 확인해 주세요)
+                        codeWrongMessage.visibility = View.VISIBLE
+                        codeWrongMessage.setTextColor(Color.RED)
+                    }
+
                 }else{
                     // 통신이 실패한 경우(응답코드 3xx, 4xx 등)
                     Log.d("post", "onResponse 실패")
