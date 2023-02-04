a367a3fc5adc87f82ea38b324049c02caf097925
yoon-gang00 <sgi07151@naver.com>
2023-02-04 15:30:33 +0900

    feat: 매칭요청 다이알로그 #36

--- app/src/main/java/com/example/fit_i/ui/match/MatchCheckActivity1.kt
+++ app/src/main/java/com/example/fit_i/ui/match/MatchCheckActivity1.kt
@@ -1,9 +1,12 @@
 package com.example.fit_i.ui.match
 
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageButton
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.example.fit_i.R
 import com.example.fit_i.databinding.ActivityMatchCheck1Binding
@@ -18,8 +21,21 @@ class MatchCheckActivity1:AppCompatActivity() {
 
         val btnNext: Button = findViewById(R.id.btn_next)
         btnNext.setOnClickListener{
-            val dialog = MatchRequestDialog(this)
-            dialog.matchDialog()
+            AlertDialog.Builder(this)
+                .setTitle("매칭요청")
+                .setMessage("매칭을 요청하시겠습니까?")
+                .setPositiveButton("요청", object : DialogInterface.OnClickListener {
+                    override fun onClick(dialog: DialogInterface, which: Int) {
+                        Log.d("MyTag", "positive")
+                    }
+                })
+                .setNegativeButton("취소", object : DialogInterface.OnClickListener {
+                    override fun onClick(dialog: DialogInterface, which: Int) {
+                        Log.d("MyTag", "negative")
+                    }
+                })
+                .create()
+                .show()
 
         }
         //뒤로가기
