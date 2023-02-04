abd583e78b19fa36877aac3e86a3bfa4e2a9e571
gurino <2021111786@dgu.ac.kr>
2023-02-05 00:45:37 +0900

    feat : 고객 매칭 명세표 api #40

--- app/src/main/java/com/example/fit_i/ui/main/matching/MatchingListActivity.kt
+++ app/src/main/java/com/example/fit_i/ui/main/matching/MatchingListActivity.kt
@@ -3,24 +3,35 @@ package com.example.fit_i.ui.main.matching
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageButton
 import com.example.fit_i.R
+import com.example.fit_i.RetrofitImpl
 import com.example.fit_i.data.model.response.GetMatchlistResponse
+import com.example.fit_i.data.service.MatchingService
 import com.example.fit_i.databinding.ActivityMatchingListBinding
 import com.example.fit_i.ui.profile.ProfileActivity
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MatchingListActivity : AppCompatActivity() {
     //나중에 정의해도 됨 lateinit
-    private lateinit var binding: ActivityMatchingListBinding
+    private var _binding: ActivityMatchingListBinding? = null
+    private val binding : ActivityMatchingListBinding
+    get() = requireNotNull(_binding){"ActivityMatchingListBinding"}
+
+    private var matchingIdx : Int = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_matching_list)
-        binding = ActivityMatchingListBinding.inflate(layoutInflater)
+        _binding = ActivityMatchingListBinding.inflate(layoutInflater)
 
         val btnmatching = findViewById<Button>(R.id.btn_matching_List)
         val btnback = findViewById<ImageButton>(R.id.btn_back)
+        val matchingService = RetrofitImpl.getApiClient().create(MatchingService::class.java)
 
 
         //뒤로 가기 버튼
@@ -34,6 +45,26 @@ class MatchingListActivity : AppCompatActivity() {
         btnmatching.setOnClickListener{
             startActivity(Intent(this,ProfileActivity::class.java))
         }
+        matchingService.matchinglist(matchingIdx).enqueue(object : Callback<GetMatchlistResponse>{
+            override fun onResponse(
+                call: Call<GetMatchlistResponse>,
+                response: Response<GetMatchlistResponse>
+            ) { if (response.isSuccessful){
+                //정상 통신
+                Log.d("post","매칭 명세표 onResponse 성공:"+ response.body().toString())
+            }
+                else{
+                    //통신실패
+                    Log.d("post","매칭 명세표 onResponse 실패:"+ response.body().toString())
+            }
+            }
+
+            override fun onFailure(call: Call<GetMatchlistResponse>, t: Throwable) {
+                //통신 실패 (예외)
+                Log.d("post"," 매칭 명세표 onFailure 에러"+ t.message.toString())
+            }
+
+        })
 
 
     }
@@ -47,4 +78,6 @@ class MatchingListActivity : AppCompatActivity() {
         binding.tvMatchingPickup2.text = data.pickUpType
         binding.tvMatchingPlace.text = data.location
     }
+
+
 }
