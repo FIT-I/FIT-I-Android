b16b70b5cb20a972f85a70c04aed8783332f328e
Yujin <jini1514@soongsil.ac.kr>
2023-02-10 14:39:53 +0900

    feat : 리뷰작성 디테일 수정 #36

--- app/src/main/java/com/example/fit_i/ui/main/mypage/review/ReviewAdapter.kt
+++ app/src/main/java/com/example/fit_i/ui/main/mypage/review/ReviewAdapter.kt
@@ -8,18 +8,27 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.fit_i.data.model.response.GetMCResponse
 import com.example.fit_i.databinding.ItemReviewBinding
 import com.example.fit_i.ui.profile.ProfileActivity
 
-class ReviewAdapter(private val dataList: List<GetMCResponse.Result>):        RecyclerView.Adapter<ReviewAdapter.ViewHolder>(){
+class ReviewAdapter(private val dataList: List<GetMCResponse.Result>):
+    RecyclerView.Adapter<ReviewAdapter.ViewHolder>(){
     inner class ViewHolder(private val binding: ItemReviewBinding):
     RecyclerView.ViewHolder(binding.root) {
 
         fun onBind(position: Int) {
-            binding.reviewIm.text = dataList[position].name
-            binding.reviewStar.text = dataList[position].grade.toString()
-            binding.reviewUni.text = dataList[position].school
+            binding.tvName.text = dataList[position].name
+            binding.tvGrade.text = dataList[position].grade.toString()
+            binding.tvSchool.text = dataList[position].school
+
+            if (dataList[position].profile != "trainerProfile"|| dataList[position].profile  != null) {
+                Glide.with(itemView)
+                    .load("${dataList[position]?.profile}")
+                    .into(binding.ivProfile)
+                Log.d("post", dataList[position].profile)
+            }
 
         }
     }
@@ -35,9 +44,9 @@ class ReviewAdapter(private val dataList: List<GetMCResponse.Result>):        Re
         holder.onBind(position)
 
         holder.itemView.setOnClickListener {
-            val intent = Intent(holder.itemView.context, ProfileActivity::class.java)
+            val intent = Intent(holder.itemView.context, MypageReviewWriteActivity::class.java)
             intent.putExtra(
-                "Id", dataList[position].trainerId)
+                "reviewIdx", dataList[position].trainerId)
             ContextCompat.startActivity(holder.itemView.context, intent, null)
         }
     }
